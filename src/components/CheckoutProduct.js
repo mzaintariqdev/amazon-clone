3c0bb51bcfb5ad040b841075ee6c9c41e4f240b3
mzaintariqdev <mzaintariqdev@gmail.com>
2022-03-18 22:42:31 +0500

    feat: basket page done

---
+++ src/components/CheckoutProduct.js
@@ -0,0 +1,63 @@
+import { StarIcon } from '@heroicons/react/solid';
+import Image from 'next/image';
+import React from 'react';
+import Currency from 'react-currency-formatter';
+import { useDispatch } from 'react-redux';
+import { addToBasket, removeFromBasket } from '../slices/basketSlice';
+function CheckoutProduct({ id, title, price, rating, description, category, image, hasPrime }) {
+  const dispatch = useDispatch();
+
+  const addItemToBasket = () => {
+    const product = {
+      id,
+      title,
+      price,
+      description,
+      category,
+      image,
+      price,
+      rating,
+      hasPrime,
+    };
+    dispatch(addToBasket(product));
+  };
+  const removeItemFromBasket = () => {
+    dispatch(removeFromBasket({ id }));
+  };
+  return (
+    <div className="grid grid-cols-5">
+      <Image src={image} height={200} width={200} objectFit="contain" />
+      {/* middle */}
+
+      <div className="col-span-3 mx-5">
+        <p>{title}</p>
+        <div className="flex">
+          {Array(rating)
+            .fill()
+            .map((_, i) => (
+              <StarIcon className="h-5 text-yellow-500" />
+            ))}
+        </div>
+        <p className="text-xs mt-2 mb-2 line-clamp-3">{description}</p>
+        <Currency quantity={price} currency="USD" />
+        {hasPrime && (
+          <div className="text-xs  my-2 line-clamp-2">
+            <img loading="lazy" className="w-12" src="https://links.papareact.com/fdw" />
+            <p className="text-xs text-gray-500">Free Next-day Delivery</p>
+          </div>
+        )}
+      </div>
+      {/* Right Add/Remove  Button*/}
+      <div className="flex flex-col space-y-2 my-auto justify-self-end">
+        <button onClick={addItemToBasket} className="button">
+          Add to Basket
+        </button>
+        <button className="button" onClick={removeItemFromBasket}>
+          Remove from Basket
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default CheckoutProduct;
